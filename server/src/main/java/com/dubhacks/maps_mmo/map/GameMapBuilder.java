dced6849cb13051995c1bf132ca40f1fdb4fa9b1
irfing <irvingc@hotmail.com>
2014-10-18 14:45:39 -0700

    Increased default resolution and removed some unused imports

--- server/src/main/java/com/dubhacks/maps_mmo/map/GameMapBuilder.java
+++ server/src/main/java/com/dubhacks/maps_mmo/map/GameMapBuilder.java
@@ -6,9 +6,7 @@ import com.dubhacks.maps_mmo.core.map.MinMaxLngLat;
 import com.dubhacks.maps_mmo.map.classifiers.*;
 import com.dubhacks.maps_mmo.map.renderers.*;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
@@ -25,7 +23,7 @@ public class GameMapBuilder {
     private byte[][] tiles;
 
     public GameMapBuilder() {
-        this(0.0001);
+        this(0.00001);
     }
     
     /**
