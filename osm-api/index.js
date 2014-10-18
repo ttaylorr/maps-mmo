bae15a6f5b129e5cc87c04a5970fabd6db72adae
Taylor Blau <me@ttaylorr.com>
2014-10-18 12:17:31 -0700

    Add /center/ endpoint to get data around a point

--- osm-api/index.js
+++ osm-api/index.js
@@ -8,11 +8,33 @@ app.get('/', function(req, res) {
   var max_lat = req.param('max_lat', 0)
     , min_lat = req.param('min_lat', 0)
     , max_lon = req.param('max_lon', 0)
-    , min_lon = req.param('min_lon', 0)
-    , coords = [min_lon,min_lat,max_lon,max_lat]
-    , url = 'http://api.openstreetmap.org/api/0.6/map?bbox='+coords.join();
+    , min_lon = req.param('min_lon', 0);
 
-  var content = "";
+  getGeoJson(max_lat, min_lat, max_lon, min_lon, res);
+});
+
+app.get('/center/', function(req, res) {
+  var center_lat = parseFloat(req.param('x'))
+    , center_lon = parseFloat(req.param('y'))
+    , radius = parseFloat(req.param('r'));
+
+  var min_lat = center_lat - radius
+    , max_lat = center_lat + radius
+    , min_lon = center_lon - radius
+    , max_lon = center_lon + radius;
+
+  getGeoJson(max_lat, min_lat, max_lon, min_lon, res);
+});
+
+var server = app.listen(3000, function() {
+  console.log("startin' this bitch on 3000");
+});
+
+function getGeoJson(max_lat, min_lat, max_lon, min_lon, res) {
+  var coords = [min_lon,min_lat,max_lon,max_lat]
+    , url = 'http://api.openstreetmap.org/api/0.6/map?bbox='+coords.join()
+    , content = "";
+  
   var a_req = http.get(url, function(_res) {
     _res.on('data', function(chunk) {
       content += chunk;
@@ -23,8 +45,4 @@ app.get('/', function(req, res) {
       res.send(o2j.osm2geojson(dom));
     });
   });
-});
-
-var server = app.listen(3000, function() {
-  console.log("startin' this bitch on 3000");
-});
+}
